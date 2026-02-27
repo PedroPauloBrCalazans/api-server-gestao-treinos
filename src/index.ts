7805e488464448ad2489656f31d74b57db88a8a2
PedroPauloBrCalazans <pedropaulo91.calazans@gmail.com>
2026-02-27 15:49:51 -0300

    feat: Add fastify api

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("Olá mund888o")
+import "dotenv/config";
+import Fastify from "fastify";
+
+const fastify = Fastify({
+  logger: true,
+});
+
+fastify.get("/", (request, reply) => {
+  return { hello: "word" };
+});
+
+try {
+  await fastify.listen({ port: Number(process.env.PORT) || 7474 });
+} catch (err) {
+  fastify.log.error(err);
+  process.exit(1);
+}
