e5ddae0d74795fb2b1a1dd751b57ea3089f7ac63
PedroPauloBrCalazans <pedropaulo91.calazans@gmail.com>
2026-03-02 12:39:43 -0300

    chore: Add fastify swagger + swagger-ui

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,51 @@
 import "dotenv/config";
 import Fastify from "fastify";
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUi from "@fastify/swagger-ui";
 
-const fastify = Fastify({
+import {
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+  ZodTypeProvider,
+} from "fastify-type-provider-zod";
+import z from "zod";
+
+const app = Fastify({
   logger: true,
 });
 
-fastify.get("/", (request, reply) => {
+app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler);
+
+await app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "Gestão Treinos",
+      description: "API para gestão de treinos",
+      version: "1.0.0",
+    },
+    servers: [
+      {
+        description: "Localhost",
+        url: "http://localhost:7474",
+      },
+    ],
+  },
+  transform: jsonSchemaTransform,
+});
+
+await app.register(fastifySwaggerUi, {
+  routePrefix: "/docs",
+});
+
+app.get("/", (request, reply) => {
   return { hello: "word" };
 });
 
 try {
-  await fastify.listen({ port: Number(process.env.PORT) || 7474 });
+  await app.listen({ port: Number(process.env.PORT) || 7474 });
 } catch (err) {
-  fastify.log.error(err);
+  app.log.error(err);
   process.exit(1);
 }
